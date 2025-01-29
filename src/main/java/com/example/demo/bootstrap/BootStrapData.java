20534bc26d05f23e8081124221be9324ea42f5c3
Nick Looney <nloone5@wgu.edu>
2025-01-29 14:46:59 -0600

    Fulfilled requirement E by adding 5 products and 5 parts to BootStrapData.java. 5 parts were added from lines 58 through 123, and new products were added from lines 133 through 145. An if statement surrounds the parts additions as well as the products to ensure they are only added if the current amount in the repositories are 0. Also fixed minor errors with the links to Home and About Us.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -55,9 +55,72 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println(thePart.getCompanyName());
         */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
+        List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+
+        if (outsourcedPartRepository.count() == 0) {
+            OutsourcedPart frame = new OutsourcedPart();
+            frame.setCompanyName("Looney's Cars");
+            frame.setName("Frame");
+            frame.setInv(10);
+            frame.setPrice(1000.0);
+            frame.setId(100L);
+            outsourcedPartRepository.save(frame);
+            OutsourcedPart thePart = null;
+            for (OutsourcedPart part:outsourcedParts) {
+                if(part.getName().equals("Frame"))thePart=part;
+            }
+
+            OutsourcedPart engine = new OutsourcedPart();
+            engine.setCompanyName("Looney's Cars");
+            engine.setName("Engine");
+            engine.setInv(7);
+            engine.setPrice(3000.0);
+            engine.setId(200L);
+            outsourcedPartRepository.save(engine);
+            thePart = null;
+            for (OutsourcedPart part:outsourcedParts) {
+                if(part.getName().equals("Engine"))thePart=part;
+            }
+
+            OutsourcedPart door = new OutsourcedPart();
+            door.setCompanyName("Looney's Cars");
+            door.setName("Door");
+            door.setInv(34);
+            door.setPrice(500.0);
+            door.setId(300L);
+            outsourcedPartRepository.save(door);
+            thePart = null;
+            for (OutsourcedPart part:outsourcedParts) {
+                if(part.getName().equals("Door"))thePart=part;
+            }
+
+            OutsourcedPart wheel = new OutsourcedPart();
+            wheel.setCompanyName("Looney's Cars");
+            wheel.setName("Wheel");
+            wheel.setInv(29);
+            wheel.setPrice(300.0);
+            wheel.setId(400L);
+            outsourcedPartRepository.save(wheel);
+            thePart = null;
+            for (OutsourcedPart part:outsourcedParts) {
+                if(part.getName().equals("Wheel"))thePart=part;
+            }
+
+            OutsourcedPart seat = new OutsourcedPart();
+            seat.setCompanyName("Looney's Cars");
+            seat.setName("Seat");
+            seat.setInv(46);
+            seat.setPrice(400.0);
+            seat.setId(250L);
+            outsourcedPartRepository.save(seat);
+            thePart = null;
+            for (OutsourcedPart part:outsourcedParts) {
+                if(part.getName().equals("Seat  "))thePart=part;
+            }
+
+            for (OutsourcedPart part : outsourcedParts) {
+                System.out.println(part.getName() + " " + part.getCompanyName());
+            }
         }
 
         /*
@@ -66,6 +129,20 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(bicycle);
         productRepository.save(unicycle);
         */
+
+        Product coupe = new Product("Coupe", 50000.0, 8);
+        Product sedan = new Product("Sedan", 40000.0, 17);
+        Product suv = new Product("SUV", 55000, 36);
+        Product truck = new Product("Truck", 62000, 25);
+        Product ev = new Product("Electric Vehicle", 70000, 22);
+
+        if (productRepository.count() == 0) {
+            productRepository.save(coupe);
+            productRepository.save(sedan);
+            productRepository.save(suv);
+            productRepository.save(truck);
+            productRepository.save(ev);
+        }
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
